2a772208777c34896ad593703423106198c4e5f3
faroyam <faroyam1@yandex.ru>
2018-08-20 21:39:45 +0700

    readme & makeCerts fixed

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # golang-p2p-chat
 
-console chat with auto reconnection written in go
+console chat with auto reconnection and TLS encryption written in go
+
+### Installing
+
+```
+go get github.com/faroyam/golang-p2p-chat
+```
+
+## Getting Started
+
+1. Generate SSL keys for TSL 
+```
+bash makeCerts.sh
+```
+2. Run chat
+```
+go build chat.go && ./chat <username> <remote IP> 
+```
--- makeCerts.sh
+++ makeCerts.sh
@@ -1,6 +1,7 @@
 mkdir certs 2>/dev/null
 rm certs/* 2>/dev/null
-echo "make server cert"
-openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=RU/ST=MSK/L=MSK/O=Chat Company/OU=IT/CN=www.chat.com/emailAddress=$1"
-echo "make client cert"
-openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=RU/ST=MSK/L=MSK/O=Chat Company/OU=IT/CN=www.chat.com/emailAddress=$1"
+echo "creating server cert"
+openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=RU/ST=MSK/L=MSK/O=Chat Company/OU=IT/CN=www.chat.com/emailAddress=example@mail.com"
+echo "creating client cert"
+openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=RU/ST=MSK/L=MSK/O=Chat Company/OU=IT/CN=www.chat.com/emailAddress=example@mail.com"
+echo "done"
